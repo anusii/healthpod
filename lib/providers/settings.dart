79972a3debfa95f78eb8a81f99fe89a265c20733
KevWindows <kevinwjh5200@gmail.com>
2025-02-26 15:04:51 +1100

    set default of serverURL

--- lib/providers/settings.dart
+++ lib/providers/settings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final serverURLProvider = StateProvider<String>((ref) => '');
+final serverURLProvider =
+    StateProvider<String>((ref) => 'https://pods.dev.solidcommunity.au');
 final usernameProvider = StateProvider<String>((ref) => '');
 final passwordProvider = StateProvider<String>((ref) => '');
 final secretKeyProvider = StateProvider<String>((ref) => '');
