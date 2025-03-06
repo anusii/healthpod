878ce24539d87082625b686ea5d329ef7323bc93
atangster <ashley.t1248@gmail.com>
2025-03-07 01:14:20 +1100

    Add additional doc and format imports

--- lib/features/file/service/components/file_upload_section.dart
+++ lib/features/file/service/components/file_upload_section.dart
@@ -1,6 +1,34 @@
+/// File upload section component for the file service feature.
+///
+// Time-stamp: <Friday 2025-02-14 08:40:39 +1100 Graham Williams>
+///
+/// Copyright (C) 2024-2025, Software Innovation Institute, ANU.
+///
+/// Licensed under the GNU General Public License, Version 3 (the "License").
+///
+/// License: https://www.gnu.org/licenses/gpl-3.0.en.html.
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <https://www.gnu.org/licenses/>.
+///
+/// Authors: Ashley Tang
+
+library;
+
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path/path.dart' as path;
