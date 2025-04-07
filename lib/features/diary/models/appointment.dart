c78529ba4532030d7e8380c54f54749357ef6c15
KevWindows <kevinwjh5200@gmail.com>
2025-04-07 10:20:24 +1000

    o add blank line after docstrings/comments

--- lib/features/diary/models/appointment.dart
+++ lib/features/diary/models/appointment.dart
@@ -31,15 +31,19 @@ library;
 /// title, description, and whether it's in the past or future.
 class Appointment {
   /// The date and time of the appointment.
+
   final DateTime date;
 
   /// The title or name of the appointment.
+
   final String title;
 
   /// A detailed description of the appointment.
+
   final String description;
 
   /// Whether the appointment is in the past.
+
   final bool isPast;
 
   /// Creates a new [Appointment] instance.
@@ -48,6 +52,7 @@ class Appointment {
   /// [title] is the title or name of the appointment.
   /// [description] is a detailed description of the appointment.
   /// [isPast] indicates whether the appointment is in the past.
+
   Appointment({
     required this.date,
     required this.title,
