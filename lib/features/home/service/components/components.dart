89821ba7015b5f25d77e09479a0c6ba65644b023
atangster <ashley.t1248@gmail.com>
2025-04-15 22:48:47 +1000

    combine name and personal details into single card

--- lib/features/home/service/components/components.dart
+++ lib/features/home/service/components/components.dart
@@ -29,9 +29,9 @@ library;
 /// This barrel file exports all the components used in the home service feature,
 /// making them easily accessible to other parts of the application.
 
-export 'avatar_name.dart';
+// Note: avatar_name.dart and personal_details.dart components are now combined into profile_details.dart
 export 'manage_plan.dart';
 export 'next_appointment.dart';
 export 'number_appointments.dart';
-export 'personal_details.dart';
+export 'profile_details.dart';
 export 'profile_management.dart';
