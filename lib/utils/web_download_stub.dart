1c1d43c5fade2ac46741aa92fd6678676500957b
atangster <ashley.t1248@gmail.com>
2025-08-05 00:23:36 +1000

    add licence headers

--- lib/utils/web_download_stub.dart
+++ lib/utils/web_download_stub.dart
@@ -1,7 +1,25 @@
 /// Stub implementation for non-web platforms.
 ///
-/// This file provides a stub implementation for file download functionality
-/// that will be used on non-web platforms where browser APIs are not available.
+/// Copyright (C) 2024-2025, Software Innovation Institute, ANU.
+///
+/// Licensed under the GNU General Public License, Version 3 (the "License").
+///
+/// License: https://www.gnu.org/licenses/gpl-3.0.en.html.
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <https://www.gnu.org/licenses/>.
+///
+/// Authors: Ashley Tang
 
 library;
 
@@ -11,6 +29,7 @@ import 'package:flutter/foundation.dart';
 ///
 /// This is a no-op implementation for non-web platforms since file downloads
 /// are handled differently (via file picker).
+
 void downloadJsonFile(String jsonContent, String fileName) {
   debugPrint('⚠️ downloadJsonFile: Stub called on non-web platform');
   // No-op on non-web platforms
