2d86ae7d58a2c01d8dcd252d85e2e0adecb008b6
Tony Chen <yang.chen2@anu.edu.au>
2026-01-22 22:00:32 +1100

    Lint

--- lib/widgets/setting_field.dart
+++ lib/widgets/setting_field.dart
@@ -137,7 +137,8 @@ class SettingField<T extends StringSettingNotifier> extends ConsumerWidget {
                             onPressed: () {
                               ref
                                   .read(
-                                      isPasswordVisibleProvider(label).notifier)
+                                    isPasswordVisibleProvider(label).notifier,
+                                  )
                                   .setState(!showPassword);
                             },
                           )
